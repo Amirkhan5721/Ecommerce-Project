aca19c7812f37743e63a507c4826347f9c3a8b14
Amirkhan5721 <amirkhanafridi959@gmail.com>
2025-04-09 10:59:55 +0500

    solve problem

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ecommerce-Project/', // ⚠️ important
+  base: '/Ecommerce-Project/', // Add this line with your repo name
 })
