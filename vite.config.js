1bcfeee58baa50a8d98c23457e4da221ba69791e
Amirkhan5721 <amirkhanafridi959@gmail.com>
2025-04-09 11:44:15 +0500

    Add and solve the error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ecommerce-Project/', // Add this line with your repo name
+  base: '/Ecommerce-Project/',
+  build: {
+    outDir: 'build', // Make this match your gh-pages deploy directory
+    assetsDir: 'assets', // Ensure assets go to the correct folder
+    emptyOutDir: true // Cleans the directory before build
+  }
 })
